0e374ba326372ff258b869a146351dfece700106
Leonardo Araujo Alvarenga <103152052+LeoAlvarengaDev@users.noreply.github.com>
2023-12-04 18:36:18 -0300

    Update main.c

--- Pratica03/atividade05/main.c
+++ Pratica03/atividade05/main.c
@@ -1,72 +1,73 @@
 #include <stdio.h>
 
-#define MAX_CITIES 4
+#define MAX_CIDADES 4
 
-int graph[MAX_CITIES][MAX_CITIES] = {0}; // Inicializa a matriz com todas as distâncias como 0
+int grafo[MAX_CIDADES][MAX_CIDADES] = {0}; // Inicializa a matriz com todas as distÃ¢ncias como 0
 
-// Procedimento para inserir a distância entre duas cidades na matriz
-void insertDistance(int city1, int city2, int distance) {
-    if (city1 >= 0 && city1 < MAX_CITIES && city2 >= 0 && city2 < MAX_CITIES) {
-        graph[city1][city2] = distance;
-        graph[city2][city1] = distance; // O grafo é não-direcionado, portanto, atualizamos ambas as direções
+// Procedimento para inserir a distÃ¢ncia entre duas cidades na matriz
+void inserirDistancia(int cidade1, int cidade2, int distancia) {
+    if (cidade1 >= 0 && cidade1 < MAX_CIDADES && cidade2 >= 0 && cidade2 < MAX_CIDADES) {
+        grafo[cidade1][cidade2] = distancia;
+        grafo[cidade2][cidade1] = distancia; // O grafo Ã© nÃ£o-direcionado, portanto, atualizamos ambas as direÃ§Ãµes
     }
 }
 
 // Procedimento para contar quantas estradas ligam as cidades
-int countRoads(int city) {
-    int count = 0;
-    for (int i = 0; i < MAX_CITIES; i++) {
-        if (graph[city][i] > 0) {
-            count++;
+int contarEstradas(int cidade) {
+    int contador = 0;
+    for (int i = 0; i < MAX_CIDADES; i++) {
+        if (grafo[cidade][i] > 0) {
+            contador++;
         }
     }
-    return count;
+    return contador;
 }
 
-// Procedimento para imprimir a matriz de distâncias
-void printGraph() {
-    for (int i = 0; i < MAX_CITIES; i++) {
-        for (int j = 0; j < MAX_CITIES; j++) {
-            printf("%d ", graph[i][j]);
+// Procedimento para imprimir a matriz de distÃ¢ncias
+void imprimirGrafo() {
+    for (int i = 0; i < MAX_CIDADES; i++) {
+        for (int j = 0; j < MAX_CIDADES; j++) {
+            printf("%d ", grafo[i][j]);
         }
         printf("\n");
     }
 }
 
-// Função recursiva para obter a distância entre duas cidades
-int getDistance(int city1, int city2) {
-    if (city1 >= 0 && city1 < MAX_CITIES && city2 >= 0 && city2 < MAX_CITIES) {
-        if (graph[city1][city2] > 0) {
-            return graph[city1][city2];
+// FunÃ§Ã£o recursiva para obter a distÃ¢ncia entre duas cidades
+int obterDistancia(int cidade1, int cidade2) {
+    if (cidade1 >= 0 && cidade1 < MAX_CIDADES && cidade2 >= 0 && cidade2 < MAX_CIDADES) {
+        if (grafo[cidade1][cidade2] > 0) {
+            return grafo[cidade1][cidade2];
         }
     }
-    return 0; // Retorna 0 se não houver estrada ligando as cidades
+    return 0; // Retorna 0 se nÃ£o houver estrada ligando as cidades
 }
 
 int main() {
-    // Inserir distâncias entre cidades
-    insertDistance(0, 1, 10); // Cidade 0 para Cidade 1: 10 km
-    insertDistance(1, 2, 15); // Cidade 1 para Cidade 2: 15 km
-    insertDistance(2, 3, 20); // Cidade 2 para Cidade 3: 20 km
+    // Inserir distÃ¢ncias entre cidades
+    inserirDistancia(0, 1, 10); // Cidade 0 para Cidade 1: 10 km
+    inserirDistancia(1, 2, 15); // Cidade 1 para Cidade 2: 15 km
+    inserirDistancia(2, 3, 20); // Cidade 2 para Cidade 3: 20 km
 
     // Contar estradas ligando cidades
-    int city = 1; // Cidade 1
-    int roadCount = countRoads(city);
-    printf("Cidade %d tem %d estradas ligando outras cidades.\n", city, roadCount);
+    int cidade = 1; // Cidade 1
+    int qtdEstradas = contarEstradas(cidade);
+    printf("Cidade %d tem %d estradas ligando outras cidades.\n", cidade, qtdEstradas);
 
-    // Imprimir a matriz de distâncias
-    printf("Matriz de Distâncias:\n");
-    printGraph();
+    // Imprimir a matriz de distÃ¢ncias
+    printf("Matriz de DistÃ¢ncias:\n");
+    imprimirGrafo();
 
-    // Obter distância entre cidades
-    int cityA = 0; // Cidade 0
-    int cityB = 2; // Cidade 2
-    int distance = getDistance(cityA, cityB);
-    if (distance > 0) {
-        printf("A distância entre Cidade %d e Cidade %d é %d km.\n", cityA, cityB, distance);
+    // Obter distÃ¢ncia entre cidades
+    int cidadeA = 0; // Cidade 0
+    int cidadeB = 2; // Cidade 2
+    int distancia = obterDistancia(cidadeA, cidadeB);
+    if (distancia > 0) {
+        printf("A distÃ¢ncia entre Cidade %d e Cidade %d Ã© %d km.\n", cidadeA, cidadeB, distancia);
     } else {
-        printf("Não há estrada ligando Cidade %d e Cidade %d.\n", cityA, cityB);
+        printf("NÃ£o hÃ¡ estrada ligando Cidade %d e Cidade %d.\n", cidadeA, cidadeB);
     }
 
     return 0;
 }
+
