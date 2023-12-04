20f1d632cc93d600e6160cea768bc93310f87856
Leonardo Araujo Alvarenga <103152052+LeoAlvarengaDev@users.noreply.github.com>
2023-12-04 18:41:50 -0300

    Update main.c

--- Pratica04/main.c
+++ Pratica04/main.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Função recursiva para determinar o tamanho (comprimento) da string
-int recursiveStringLength(const char *str, int index) {
+// FunÃ§Ã£o recursiva para determinar o tamanho (comprimento) da string
+int ComprimentoDaStringRecursiva(const char *str, int index) {
     if (str[index] == '\0') {
         return index;
     }
-    return recursiveStringLength(str, index + 1);
+    return ComprimentoDaStringRecursiva(str, index + 1);
 }
 
-// Função recursiva para determinar o reverso da string
-void recursiveReverseString(const char *str, char *result, int index) {
+// FunÃ§Ã£o recursiva para determinar o reverso da string
+void StringReversaRecursiva(const char *str, char *result, int index) {
     if (str[index] == '\0') {
         result[index] = '\0';
         return;
     }
 
-    recursiveReverseString(str, result, index + 1);
+    StringReversaRecursiva(str, result, index + 1);
     result[strlen(str) - index - 1] = str[index];
 }
 
 int main() {
     FILE *file = fopen("texto.txt", "r");
     if (file == NULL) {
-        printf("Não foi possível abrir o arquivo.\n");
+        printf("NÃ£o foi possÃ­vel abrir o arquivo.\n");
         return 1;
     }
 
@@ -33,27 +33,27 @@ int main() {
     fclose(file);
 
     // Calcular o tamanho da string recursivamente
-    int length = recursiveStringLength(str, 0);
+    int comprimento = ComprimentoDaStringRecursiva(str, 0);
 
     // Calcular o reverso da string recursivamente
-    char reversed[100];
-    recursiveReverseString(str, reversed, 0);
+    char invertida[100];
+    StringReversaRecursiva(str, reversed, 0);
 
     // Abrir o arquivo novamente para escrever os resultados
     file = fopen("texto.txt", "w");
     if (file == NULL) {
-        printf("Não foi possível abrir o arquivo para escrita.\n");
+        printf("NÃ£o foi possÃ­vel abrir o arquivo para escrita.\n");
         return 1;
     }
 
     // Escrever o tamanho e o reverso da string no arquivo
-    fprintf(file, "Tamanho da string: %d\n", length);
-    fprintf(file, "Reverso da string: %s\n", reversed);
+    fprintf(file, "Tamanho da string: %d\n", comprimento);
+    fprintf(file, "Reverso da string: %s\n", invertida);
 
     fclose(file);
 
-    printf("Tamanho da string: %d\n", length);
-    printf("Reverso da string: %s\n", reversed);
+    printf("Tamanho da string: %d\n", comprimento);
+    printf("Reverso da string: %s\n", invertida);
 
     return 0;
 }
